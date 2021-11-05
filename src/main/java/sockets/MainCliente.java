1d4dc50d4630a676334788cf6eb33a9f708a3221
Juan Esteban Moncada <juanestebanmb@ufps.edu.co>
2021-11-04 22:17:19 -0500

    Java socket example

---
+++ src/main/java/sockets/MainCliente.java
@@ -0,0 +1,15 @@
+package sockets;
+import java.io.IOException;
+
+
+//Clase principal que hará uso del cliente
+public class MainCliente
+{
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+}
